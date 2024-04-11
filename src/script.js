ea1c1ca5d546883ea1bb9cd5bea0a7e1ffb4b33d
lico9292 <linus.colling.9292@student.uu.se>
2024-04-11 10:35:32 +0200

    Setup: Added MQ into prototype
    Added the src folder with the prototype from branch feature/texteditor_functions and implemented MQ into that.

---
+++ src/script.js
@@ -0,0 +1,50 @@
+const textarea = document.getElementById("textarea");
+
+
+//Detta gör att man kan skriva i textarea-diven 
+window.addEventListener('load', function() {
+    textarea.setAttribute('contenteditable', 'true');
+});
+
+
+function font_size(e) {
+    let value = e.value;
+    textarea.style.fontSize = value + "px";
+}
+
+function text_bold(e) {
+    textarea.style.fontWeight = textarea.style.fontWeight === 'bold' ? 'normal' : 'bold';
+}
+
+function text_italic(e) {
+    textarea.style.fontStyle = textarea.style.fontStyle === 'italic' ? 'normal' : 'italic';
+}
+
+function text_underline(e) {
+    textarea.style.textDecoration = textarea.style.textDecoration === 'underline' ? 'none' : 'underline';
+}
+
+function align_left(e) {
+    textarea.style.textAlign = 'left';
+}
+
+function align_center(e) {
+    textarea.style.textAlign = 'center';
+}
+
+function align_right(e) {
+    textarea.style.textAlign = 'right';
+}
+
+function text_lower_or_upper(e) {
+    textarea.style.textTransform = textarea.style.textTransform === 'uppercase' ? 'none' : 'uppercase';
+}
+
+function text_slash(e) {
+    //TODO: en funktion som stryker igenom texten
+}
+
+function text_color(e) {
+    let value = e.value;
+    textarea.style.color = value;
+}
