15da50b427079465acf93ee339dcb8fd8fbe2556
Krijan <102483866+KriaPI@users.noreply.github.com>
2024-04-12 13:04:13 +0200

    Setup: added Webpack, Webpack-devserver, npm and Prettier
    * Setup: added webpack and npm
    
    Added webpack along with a development server and npm as a package
    manager. Also added npm commands to run the server and build the
    project. To run the server, type "npm run serve", and type "npm run
    build" to build.
    
    HTML and CSS files still need to be in the dist folder, but
    should eventually be placed within ./src. For this to work, webpack will
    most likely need another loader. Also note that things such as a
    formater and a linter such as ESlint is missing.
    
    * Setup: moved HTML and CSS files to ./src
    
    * Setup: added prettier

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.ts',
+    devServer: {
+        static: './dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                include: [path.resolve(__dirname, 'src')]
+            },
+            {
+                test: /\.css/,
+                type: 'asset/resource',
+                include: [path.resolve(__dirname, 'src')]
+            }
+        ]
+    },
+    output: {
+        publicPath: 'auto',
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: './src/index.html'
+    })]
+}
