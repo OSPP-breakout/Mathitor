b4d7af99d00ac093d6640714250823caa276405b
KriaPia <kristoffer.berg8@gmail.com>
2024-04-12 10:29:24 +0200

    Setup: moved HTML and CSS files to ./src

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,2 @@
 console.log('good nom');
 console.log('lol');
-
-const area = document.getElementById("textarea")!;
-
-function text_bold(e: any): void {
-
-}
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -12,12 +13,21 @@ module.exports = {
                 test: /\.ts$/,
                 use: 'ts-loader',
                 include: [path.resolve(__dirname, 'src')]
+            },
+            {
+                test: /\.css/,
+                type: 'asset/resource',
+                include: [path.resolve(__dirname, 'src')]
             }
         ]
     },
     output: {
         publicPath: 'auto',
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    }
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: './src/index.html'
+    })]
 }
