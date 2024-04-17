77af69da47d806db1c0db076ab184ba310dd006d
lico9292 <linus.colling.9292@student.uu.se>
2024-04-17 10:26:25 +0200

    Bugfix: more problems with Webpack
    Added event listeners to all objects, fixed a bug where listeners were triggered twice because of bubbling,  reordered script import because of MathQuill ReferenceError, and some other minor stuff.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,28 @@
-//declare var MathQuill: any;
-//
+declare var MathQuill: any;
+
 const textarea = document.getElementById("textarea") as HTMLElement;
-//const latexSpan = document.getElementById('latex') as HTMLElement;
-//const MQ = MathQuill.getInterface(2);
-//var amountOfMQFields = 0;
+const latexSpan = document.getElementById('latex') as HTMLElement;
+const MQ = MathQuill.getInterface(2);
+var amountOfMQFields = 0;
 
+//TODO: Extract all addEventListener call, something like this maybe:
+/*
+type triggerFunction = (a: any) => any; 
+function createEventListener(elementID: string, func: triggerFunction): void {
+    document.getElementById(elementID)?.addEventListener("click", function(event) {
+        func;
+        event.stopImmediatePropagation();
+    }, false);
+}
+*/
 
-// TODO: add event listeners to all other buttons
-document.getElementById("font-size")?.addEventListener("change", font_size);
+// So then you could just do this for every element
+// createEventListener("button-bold", text_bold);
+
+document.getElementById("font-size")?.addEventListener("change", function(event) {
+    font_size(event);
+    event.stopImmediatePropagation();
+}, false);
 
 function font_size(e: any): void {
     let value = e.target.value;
@@ -15,61 +30,120 @@ function font_size(e: any): void {
     textarea.style.fontSize = value + "px";
 }
 
-function text_bold(e: any): void {
+
+document.getElementById("btn-bold")?.addEventListener("click", function(event) {
+    text_bold();
+    event.stopImmediatePropagation();
+}, false);
+
+function text_bold(): void {
     textarea.style.fontWeight = textarea.style.fontWeight === 'bold' ? 'normal' : 'bold';
 }
 
-function text_italic(e: any): void {
+
+document.getElementById("btn-italic")?.addEventListener("click", function(event) {
+    text_italic();
+    event.stopImmediatePropagation();
+}, false);
+
+function text_italic(): void {
     textarea.style.fontStyle = textarea.style.fontStyle === 'italic' ? 'normal' : 'italic';
 }
 
-function text_underline(e: any): void {
+
+document.getElementById("btn-underline")?.addEventListener("click", function(event) {
+    text_underline();
+    event.stopImmediatePropagation();
+}, false);
+
+function text_underline(): void {
     textarea.style.textDecoration = textarea.style.textDecoration === 'underline' ? 'none' : 'underline';
 }
 
-function align_left(e: any): void {
+
+document.getElementById("btn-align-l")?.addEventListener("click", function(event) {
+    align_left();
+    event.stopImmediatePropagation();
+}, false);
+
+function align_left(): void {
     textarea.style.textAlign = 'left';
 }
 
-function align_center(e: any): void {
+
+document.getElementById("btn-align-c")?.addEventListener("click", function(event) {
+    align_center();
+    event.stopImmediatePropagation();
+}, false);
+
+function align_center(): void {
     textarea.style.textAlign = 'center';
 }
 
-function align_right(e: any): void {
+
+document.getElementById("btn-align-r")?.addEventListener("click", function(event) {
+    align_right();
+    event.stopImmediatePropagation();
+}, false);
+
+function align_right(): void {
     textarea.style.textAlign = 'right';
 }
 
-function text_lower_or_upper(e: any): void {
+
+document.getElementById("lower-upper")?.addEventListener("click", function(event) {
+    text_lower_or_upper();
+    event.stopImmediatePropagation();
+}, false);
+
+function text_lower_or_upper(): void {
     textarea.style.textTransform = textarea.style.textTransform === 'uppercase' ? 'none' : 'uppercase';
 }
 
-function text_slash(e: any): void {
+
+document.getElementById("btn-slash")?.addEventListener("click", function(event) {
+    text_slash();
+    event.stopImmediatePropagation();
+}, false);
+
+function text_slash(): void {
     //TODO: en funktion som stryker igenom texten
 }
 
+
+document.getElementById("color-picker")?.addEventListener("change", function(event) {
+    text_color(event);
+    event.stopImmediatePropagation();
+}, false);
+
 function text_color(e: any): void {
-    let value = e.value;
+    let value = e.target.value;
     textarea.style.color = value;
 }
 
-//function create_MQ_field(): void {
-//    amountOfMQFields++;
-//    var newMQField = document.createElement('math-field' + amountOfMQFields.toString());
-//    
-//    //Insert the new field at caret position
-//    var range = (window.getSelection() as Selection).getRangeAt(0);
-//
-//    if ((range.startContainer.parentNode as Element).id ==='textarea') {
-//        range.insertNode(newMQField);
-//    }
-//
-//    //Link the new MQ-field to the latex-preview  
-//    var mathField = MQ.MathField(newMQField, {
-//        handlers: {
-//            edit: function () {
-//                latexSpan.textContent = mathField.latex();
-//            },
-//        },
-//    });
-//
-//}
+
+document.getElementById("button-MQ")?.addEventListener("click", function(event) {
+    create_MQ_field();
+    event.stopImmediatePropagation();
+}, false);
+
+function create_MQ_field(): void {
+   amountOfMQFields++;
+   var newMQField = document.createElement('math-field' + amountOfMQFields.toString());
+   
+   //Insert the new field at caret position
+   var range = (window.getSelection() as Selection).getRangeAt(0);
+
+   if ((range.startContainer.parentNode as Element).id ==='textarea') {
+       range.insertNode(newMQField);
+   }
+
+   //Link the new MQ-field to the latex-preview  
+   var mathField = MQ.MathField(newMQField, {
+       handlers: {
+           edit: function () {
+               latexSpan.textContent = mathField.latex();
+           },
+       },
+   });
+}
