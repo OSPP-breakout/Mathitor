2b2da2c09f773599c6fadcfd9424cb9183089a7d
wipa0947 <william.paradell@gmail.com>
2024-04-25 17:48:46 +0200

    Other: Fixed it so that window is maximized instead of fullscreen

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,11 @@ const { app, BrowserWindow } = require('electron')
 
 const createWindow = () => {
     const window = new BrowserWindow({
-      fullscreen: true
     })
     
     window.loadFile('dist/index.html');
-    window.setFullScreen(1);
+    window.maximize();
+    window.show();
     //add_listeners();
 }
 
