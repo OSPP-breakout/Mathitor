0b779ab1f24d4d9e126b3d8f6bcde811d74b9be0
limpan <linus.colling9292@student.uu.se>
2024-05-01 10:55:40 +0200

    Test: added basic functionality tests

--- test/specs/test.e2e.ts
+++ test/specs/test.e2e.ts
@@ -18,14 +18,14 @@ describe('Test section title', () => {
 
 // Text area basic functionality
 
-describe("Arrowkeys work", () => {
-    it("Tests that navigation by arrowkeys is working in text area", async () => {
+describe("Basic functionality", () => {
+    it("Navigation by left/right arrowkeys is working in text area", async () => {
         await browser.url("file:///C:/Users/linus/Mathitor/dist/index.html");
         await expect(browser).toHaveUrl("file:///C:/Users/linus/Mathitor/dist/index.html");
         const textArea = await $("#textarea");
         
         await browser.$("#textarea").click();
-        await browser.keys(['a', 'b']);
+        await browser.keys('ab');
         await expect(await textArea.getText()).toEqual("ab");
         
         await browser.keys([Key.ArrowLeft, 'c']);
@@ -34,7 +34,143 @@ describe("Arrowkeys work", () => {
         await browser.keys([Key.ArrowRight, Key.ArrowRight, 'd']);
         await expect(await textArea.getText()).toEqual("acbd")
         
-        console.log("HTML SER UT SÅHÄR: \n" + await textArea.getText() + "\n SLUT");
+        // console.log("HTML SER UT SÅHÄR: \n" + await textArea.getText() + "\n SLUT");
+    });
+    it('Undo/redo buttons', async () => {
+        await browser.url("file:///C:/Users/linus/Mathitor/dist/index.html");
+        await expect(browser).toHaveUrl("file:///C:/Users/linus/Mathitor/dist/index.html");
+        const textArea = await $("#textarea");
+        await browser.$("#textarea").click();
+        await browser.keys('ab');
+        await expect(await textArea.getText()).toEqual("ab");
+
+        await browser.$("#btn-undo").click();
+        await expect(await textArea.getText()).toEqual("");
+
+        await browser.$("#btn-redo").click();
+        await expect(await textArea.getText()).toEqual("ab");
+    });
+    it('Bold button', async () => {
+        await browser.url("file:///C:/Users/linus/Mathitor/dist/index.html");
+        await expect(browser).toHaveUrl("file:///C:/Users/linus/Mathitor/dist/index.html");
+        const textArea = await $("#textarea");
+        await browser.$("#textarea").click();
+        await browser.keys('Ett två tre');
+        await browser.keys([Key.ArrowLeft, Key.ArrowLeft, Key.ArrowLeft, Key.ArrowLeft, Key.Shift, Key.ArrowLeft, Key.ArrowLeft, Key.ArrowLeft, Key.Shift]);
+        await browser.$("#btn-bold").click();
+        await expect(await textArea.getHTML(false)).toEqual("Ett <b>två</b> tre");        
+    });
+    it('Italic button', async () => {
+        await browser.url("file:///C:/Users/linus/Mathitor/dist/index.html");
+        await expect(browser).toHaveUrl("file:///C:/Users/linus/Mathitor/dist/index.html");
+        const textArea = await $("#textarea");
+        await browser.$("#textarea").click();
+        await browser.keys('Ett två tre');
+        await browser.keys([Key.ArrowLeft, Key.ArrowLeft, Key.ArrowLeft, Key.ArrowLeft, Key.Shift, Key.ArrowLeft, Key.ArrowLeft, Key.ArrowLeft, Key.Shift]);
+        await browser.$("#btn-italic").click();
+        await expect(await textArea.getHTML(false)).toEqual("Ett <i>två</i> tre");
+        // console.log("HTML SER UT SÅHÄR: \n" + await $("#textarea").getHTML(false) + "\n SLUT");
+    });
+    it('Underline button', async () => {
+        await browser.url("file:///C:/Users/linus/Mathitor/dist/index.html");
+        await expect(browser).toHaveUrl("file:///C:/Users/linus/Mathitor/dist/index.html");
+        const textArea = await $("#textarea");
+        await browser.$("#textarea").click();
+        await browser.keys('Ett två tre');
+        await browser.keys([Key.ArrowLeft, Key.ArrowLeft, Key.ArrowLeft, Key.ArrowLeft, Key.Shift, Key.ArrowLeft, Key.ArrowLeft, Key.ArrowLeft, Key.Shift]);
+        await browser.$("#btn-underline").click();
+        await expect(await textArea.getHTML(false)).toEqual("Ett <u>två</u> tre");        
+    });
+    it('Strikethrough button', async () => {
+        await browser.url("file:///C:/Users/linus/Mathitor/dist/index.html");
+        await expect(browser).toHaveUrl("file:///C:/Users/linus/Mathitor/dist/index.html");
+        const textArea = await $("#textarea");
+        await browser.$("#textarea").click();
+        await browser.keys('Ett två tre');
+        await browser.keys([Key.ArrowLeft, Key.ArrowLeft, Key.ArrowLeft, Key.ArrowLeft, Key.Shift, Key.ArrowLeft, Key.ArrowLeft, Key.ArrowLeft, Key.Shift]);
+        await browser.$("#btn-slash").click();
+        await expect(await textArea.getHTML(false)).toEqual("Ett <strike>två</strike> tre");        
+    }); 
+    it('Align left button', async () => {
+        await browser.url("file:///C:/Users/linus/Mathitor/dist/index.html");
+        await expect(browser).toHaveUrl("file:///C:/Users/linus/Mathitor/dist/index.html");
+        const textArea = await $("#textarea");
+        await browser.$("#textarea").click();
+        await browser.keys('Ett två tre');
+        await browser.$("#textarea").click();
+        await browser.$("#btn-align-r").click();
+        await expect(await textArea.getHTML(false) as string).toEqual("<div style=\"text-align: right;\">Ett två tre</div>");
+
+        await browser.$("#btn-align-l").click();
+        await expect(await textArea.getHTML(false) as string).toEqual("<div style=\"text-align: left;\">Ett två tre</div>");
+    });
+    it('Align center button', async () => {
+        await browser.url("file:///C:/Users/linus/Mathitor/dist/index.html");
+        await expect(browser).toHaveUrl("file:///C:/Users/linus/Mathitor/dist/index.html");
+        const textArea = await $("#textarea");
+        await browser.$("#textarea").click();
+        await browser.keys('Ett två tre');
+        await browser.$("#textarea").click();
+        await browser.$("#btn-align-c").click();
+        await expect(await textArea.getHTML(false) as string).toEqual("<div style=\"text-align: center;\">Ett två tre</div>"); 
+    });
+    it('Align right button', async () => {
+        await browser.url("file:///C:/Users/linus/Mathitor/dist/index.html");
+        await expect(browser).toHaveUrl("file:///C:/Users/linus/Mathitor/dist/index.html");
+        const textArea = await $("#textarea");
+        await browser.$("#textarea").click();
+        await browser.keys('Ett två tre');
+        await browser.$("#textarea").click();
+        await browser.$("#btn-align-r").click();
+        // console.log("HTML SER UT SÅHÄR: \n" + (await textArea.getHTML(false)) + "\n SLUT");   
+        await expect(await textArea.getHTML(false) as string).toEqual("<div style=\"text-align: right;\">Ett två tre</div>"); 
+    });
+    // it('Justify full button', async () => {
+    //     await browser.url("file:///C:/Users/linus/Mathitor/dist/index.html");
+    //     await expect(browser).toHaveUrl("file:///C:/Users/linus/Mathitor/dist/index.html");
+    //     await expect("Not impemented").toEqual(false); 
+    // });
+    it('Ordered list button', async () => {
+        await browser.url("file:///C:/Users/linus/Mathitor/dist/index.html");
+        await expect(browser).toHaveUrl("file:///C:/Users/linus/Mathitor/dist/index.html");
+        const textArea = await $("#textarea");
+        await browser.$("#textarea").click();
+        await browser.keys('Ett två tre');
+        await browser.$("#textarea").click();
+        await browser.$("#btn-ordered-list").click();
+        await expect(await textArea.getHTML(false) as string).toEqual("<ol><li>Ett två tre</li></ol>"); 
+    });
+    it('Unordered list button', async () => {
+        await browser.url("file:///C:/Users/linus/Mathitor/dist/index.html");
+        await expect(browser).toHaveUrl("file:///C:/Users/linus/Mathitor/dist/index.html");
+        const textArea = await $("#textarea");
+        await browser.$("#textarea").click();
+        await browser.keys('Ett två tre');
+        await browser.$("#textarea").click();
+        await browser.$("#btn-unordered-list").click();
+        await expect(await textArea.getHTML(false) as string).toEqual("<ul><li>Ett två tre</li></ul>"); 
+    });
+    // it('Add link button', async () => {
+    //     await browser.url("file:///C:/Users/linus/Mathitor/dist/index.html");
+    //     await expect(browser).toHaveUrl("file:///C:/Users/linus/Mathitor/dist/index.html");
+    //     await expect("Not impemented").toEqual(false); 
+    // });
+    // it('Unlink button', async () => {
+    //     await browser.url("file:///C:/Users/linus/Mathitor/dist/index.html");
+    //     await expect(browser).toHaveUrl("file:///C:/Users/linus/Mathitor/dist/index.html");
+    //     await expect("Not impemented").toEqual(false); 
+    // });
+    it('Nested formatting', async () => {
+        await browser.url("file:///C:/Users/linus/Mathitor/dist/index.html");
+        await expect(browser).toHaveUrl("file:///C:/Users/linus/Mathitor/dist/index.html");
+        const textArea = await $("#textarea");
+        await browser.$("#textarea").click();
+        await browser.keys('Lorem ipsum dolor sit amet.');
+        await browser.keys([Key.Home, Key.Shift, Key.ArrowRight, Key.ArrowRight, Key.ArrowRight, Key.ArrowRight, Key.ArrowRight, Key.Shift]);
+        await browser.$("#btn-bold").click();
+        await browser.$("#btn-italic").click();
+        await browser.$("#btn-underline").click();
+        await expect(await textArea.getHTML(false)).toEqual("<b><i><u>Lorem</u></i></b> ipsum dolor sit amet.");        
     });
   });
 
@@ -42,7 +178,7 @@ describe("Arrowkeys work", () => {
 // Math field
 
 describe('Basic math field functionality', () => {
-    it('Can create and edit mathfield', async () => {
+    it('Create and edit mathfield', async () => {
         await browser.url("file:///C:/Users/linus/Mathitor/dist/index.html");
         await expect(browser).toHaveUrl("file:///C:/Users/linus/Mathitor/dist/index.html");
         const textArea = await $("#textarea");
@@ -54,7 +190,7 @@ describe('Basic math field functionality', () => {
         await browser.keys('a');
         await expect(await textArea.getText()).toEqual(" a ");
     });
-    it('Can handle MQ-commands', async () => {
+    it('Handle MQ-commands', async () => {
         await browser.url("file:///C:/Users/linus/Mathitor/dist/index.html");
         await expect(browser).toHaveUrl("file:///C:/Users/linus/Mathitor/dist/index.html");
         const textArea = await $("#textarea");
@@ -68,7 +204,7 @@ describe('Basic math field functionality', () => {
 });
 
 describe("MQ field navigation right/left", () => {
-    it("Tests navigation with right/left ArrowKeys through a mathfield", async () => {
+    it("Navigation with right/left ArrowKeys through a mathfield", async () => {
         await browser.url("file:///C:/Users/linus/Mathitor/dist/index.html");
         await expect(browser).toHaveUrl("file:///C:/Users/linus/Mathitor/dist/index.html");
         const textArea = await $("#textarea");
@@ -89,9 +225,9 @@ describe("MQ field navigation right/left", () => {
         await browser.keys([Key.ArrowLeft, "n", Key.ArrowRight, Key.ArrowRight, Key.ArrowRight, Key.ArrowRight, "o", Key.ArrowRight, Key.ArrowRight, Key.ArrowRight, Key.ArrowRight, Key.ArrowRight, Key.ArrowRight, "p"]);
         await expect(await textArea.getText()).toEqual("aanh \n∫\nbo\na\ncm lapka");
 
-        console.log("HTML SER UT SÅHÄR: \n" + await textArea.getText() + "\n SLUT");
+        // console.log("HTML SER UT SÅHÄR: \n" + await textArea.getText() + "\n SLUT");
     });
-    it('Can exit to empty right', async () => {
+    it('Exit mathfield to empty right', async () => {
         await browser.url("file:///C:/Users/linus/Mathitor/dist/index.html");
         await expect(browser).toHaveUrl("file:///C:/Users/linus/Mathitor/dist/index.html");
         const textArea = await $("#textarea");
